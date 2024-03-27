c200c6afed6910d9dddd811b4ad429a33c1dc6e6
Paresh Chavda <paresh.chavda1432@gmail.com>
2024-03-27 14:41:59 +0530

    zoom animation

--- App.tsx
+++ App.tsx
@@ -2,25 +2,26 @@ import React, {useState} from 'react';
 import {SafeAreaView, StyleSheet, View} from 'react-native';
 import LikeButton from './src/component/likeButton';
 import Popup from './src/component/popup';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const App = () => {
   const [modalVisible, setModalVisible] = useState(false);
 
-  const toggleModal = () => {
-    setModalVisible(!modalVisible);
-  };
+  const toggleModal = () => setModalVisible(!modalVisible);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={{flexDirection: 'row', position: 'absolute', bottom: 60}}>
-        <LikeButton onLongPress={toggleModal} />
-        <LikeButton onLongPress={toggleModal} />
-        <LikeButton onLongPress={toggleModal} />
-        <LikeButton onLongPress={toggleModal} />
-        <LikeButton onLongPress={toggleModal} />
-      </View>
-      <Popup modalVisible={modalVisible} onClose={toggleModal} />
-    </SafeAreaView>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <SafeAreaView style={styles.container}>
+        <View style={{flexDirection: 'row', position: 'absolute', bottom: 60}}>
+          <LikeButton onLongPress={toggleModal} />
+          <LikeButton onLongPress={toggleModal} />
+          <LikeButton onLongPress={toggleModal} />
+          <LikeButton onLongPress={toggleModal} />
+          <LikeButton onLongPress={toggleModal} />
+        </View>
+        <Popup modalVisible={modalVisible} onClose={toggleModal} />
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 };
 
--- src/component/popup.tsx
+++ src/component/popup.tsx
@@ -1,31 +1,62 @@
-import React from 'react';
-import {View, Text, Modal, StyleSheet, FlatList} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Modal,
+  StyleSheet,
+  FlatList,
+  Animated,
+  TouchableOpacity,
+  Pressable,
+} from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Feather from 'react-native-vector-icons/Feather';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const PopUp = ({modalVisible = false, onClose = () => {}}) => {
   const data = [
     {
-      title: '',
-      image: <Icon name="heart" size={24} color="black" />,
+      title: 'Like',
+      image: <FontAwesome name="thumbs-up" size={24} color="black" />,
     },
     {
-      title: '',
+      title: 'Celebrate',
       image: <MaterialIcons name="celebration" size={24} color="black" />,
     },
     {
-      title: '',
-      image: <MaterialIcons name="heart" size={24} color="black" />,
+      title: 'Support',
+      image: <MaterialIcons name="support" size={24} color="black" />,
     },
     {
-      title: '',
-      image: <MaterialIcons name="person" size={24} color="black" />,
+      title: 'Love',
+      image: (
+        <MaterialCommunityIcons name="cards-heart" size={24} color="black" />
+      ),
     },
     {
-      title: '',
-      image: <MaterialIcons name="person" size={24} color="black" />,
+      title: 'Funny',
+      image: <Feather name="smile" size={24} color="black" />,
     },
   ];
+
+  const [scaleValues] = useState(data.map(() => new Animated.Value(1)));
+
+  const handlePressIn = (index: number) => {
+    Animated.spring(scaleValues[index], {
+      toValue: 1.2, // Zoom in factor
+      friction: 3, // Adjust the speed of the animation
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const handlePressOut = (index: number) => {
+    Animated.spring(scaleValues[index], {
+      toValue: 1, // Return to original size
+      friction: 3,
+      useNativeDriver: true,
+    }).start();
+  };
 
   return (
     <View style={styles.container}>
@@ -34,24 +65,33 @@ const PopUp = ({modalVisible = false, onClose = () => {}}) => {
         transparent={true}
         visible={modalVisible}
         onRequestClose={onClose}>
-        <View style={styles.centeredView}>
+        <Pressable onPress={onClose} style={styles.centeredView}>
           <View style={styles.modalView}>
             <FlatList
               data={data}
               horizontal
+              keyExtractor={(item, index) => index.toString()}
               renderItem={({item, index}) => (
-                <Icon
-                  onPress={onClose}
-                  name="heart"
-                  size={24}
-                  color="red"
-                  style={{margin: 10}}
-                />
+                <TouchableOpacity
+                  onPressIn={() => handlePressIn(index)}
+                  onPressOut={() => handlePressOut(index)}
+                  activeOpacity={1}>
+                  <Animated.View
+                    style={[
+                      styles.zoomIn,
+                      {transform: [{scale: scaleValues[index]}]},
+                    ]}>
+                    {item.image}
+                    <Text style={{color: '#000', fontSize: 14}}>
+                      {item.title}
+                    </Text>
+                  </Animated.View>
+                </TouchableOpacity>
               )}
               contentContainerStyle={{padding: 10}}
             />
           </View>
-        </View>
+        </Pressable>
       </Modal>
     </View>
   );
@@ -64,9 +104,10 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
   },
   centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    // alignItems: 'center',
+    position: 'absolute',
+    bottom: 120,
+    left: 0,
+    right: 0,
   },
   modalView: {
     backgroundColor: 'white',
@@ -81,6 +122,12 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
+  },
+  zoomIn: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 5,
+    padding: 5,
   },
 });
 
